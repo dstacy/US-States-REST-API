698761164a9db28211f6fa64870b7be0e13f4a30
Darcy Stacy <d_stacy@mail.fhsu.edu>
2022-04-28 09:55:28 -0400

    removed cors options

--- middleware/verifyContig.js
+++ middleware/verifyContig.js
@@ -6,11 +6,9 @@ const data = {
 const verifyContig = (req, res, next) => {
     const contig = req.query.contig;
     if(contig === 'true') {
-        console.log("true");
         const states = data.states.filter(state => state.code !== 'AK' && state.code !== 'HI');
         res.states = states;
     } else if(contig === 'false') {
-        console.log("false");
         const states = data.states.filter(state => state.code === 'AK' || state.code === 'HI');
         res.states = states;
     } else {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
+// const corsOptions = require('./config/corsOptions');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3500;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 app.use('/', require('./routes/root'));
