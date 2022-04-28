d88e2b63bb3ce7fa6cb5269c567ae9850909dff2
Darcy Stacy <d_stacy@mail.fhsu.edu>
2022-04-27 21:45:24 -0400

    changed db connect info

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(process.env.DATABASE_URI);
+        /*, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
+        */
     } catch (err) {
         console.error(err);
     }
