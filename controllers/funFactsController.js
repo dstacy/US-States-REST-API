798be1439a5a2a789688e116184737cf685207e5
Darcy Stacy <d_stacy@mail.fhsu.edu>
2022-05-03 21:27:19 -0400

    updated verbage in error message

--- controllers/funFactsController.js
+++ controllers/funFactsController.js
@@ -43,9 +43,10 @@ const updateFunFact = async (req, res) => {
         try {
             const index = req.body.index - 1;  
             const facts = await States.findOne({stateCode: res.state.code}, 'funfacts').exec();
-                if(!facts) {
+            
+            if(facts === null) {
                     return res.status(404).json({ "message": `No Fun Facts found for ${res.state.state}` });
-                }
+            }
             
             const result = await States.findOneAndUpdate(
                     {stateCode: res.state.code,
@@ -54,20 +55,8 @@ const updateFunFact = async (req, res) => {
                     {new: true}
                 );
             
-        /*
-            const getMongo = await States.find({stateCode: res.state.code}, `funfacts.${index}`).exec();
-                if(getMongo === null) {
-                    return res.status(404).json({ "message": `No Fun Fact found at index ${index} for ${res.state.state}` });
-                }
-                
-            const result = await States.updateOne(
-                {stateCode: res.state.code},
-                {$set: { [`funfacts.${index}`]: req.body.funfacts.toString()}},  
-                {new: true}
-            );
-            */
             if(result === null) {
-                return res.status(404).json({ "message": `No Fun Fact found at index ${index} for ${res.state.state}` });
+                return res.status(404).json({ "message": `No Fun Fact found at that index for ${res.state.state}` });
             }else {
                 res.status(201).json(result);
             }
